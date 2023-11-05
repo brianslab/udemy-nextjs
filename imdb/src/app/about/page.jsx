fbc1dad7d7dc91e8ceb2bfa69cdd4882909add67
brianslab <bdkuhn602@gmail.com>
2023-11-04 23:52:11 -0700

    create about page

---
+++ imdb/src/app/about/page.jsx
@@ -0,0 +1,46 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Fringilla ut morbi
+        tincidunt augue interdum velit euismod in. Congue nisi vitae suscipit
+        tellus mauris. Diam ut venenatis tellus in. Venenatis lectus magna
+        fringilla urna porttitor rhoncus dolor purus non. Viverra maecenas
+        accumsan lacus vel facilisis. Dolor morbi non arcu risus quis. Laoreet
+        suspendisse interdum consectetur libero id faucibus nisl. Pellentesque
+        diam volutpat commodo sed egestas egestas fringilla. At imperdiet dui
+        accumsan sit amet nulla facilisi. Quis vel eros donec ac odio tempor. A
+        iaculis at erat pellentesque. Nec feugiat nisl pretium fusce id velit.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames. Aenean pharetra magna ac placerat vestibulum. Aliquam vestibulum
+        morbi blandit cursus risus. Et malesuada fames ac turpis egestas integer
+        eget. Quis risus sed vulputate odio ut enim blandit.
+      </p>
+      <p>
+        Tincidunt eget nullam non nisi. Consequat mauris nunc congue nisi vitae
+        suscipit tellus mauris. Orci porta non pulvinar neque laoreet
+        suspendisse interdum consectetur libero. Suspendisse sed nisi lacus sed
+        viverra tellus. Massa vitae tortor condimentum lacinia. At tempor
+        commodo ullamcorper a lacus vestibulum sed. Elit ullamcorper dignissim
+        cras tincidunt lobortis. Amet volutpat consequat mauris nunc congue nisi
+        vitae suscipit tellus. Eget nunc scelerisque viverra mauris in aliquam
+        sem. Augue neque gravida in fermentum et sollicitudin. Gravida arcu ac
+        tortor dignissim convallis aenean et. Quis enim lobortis scelerisque
+        fermentum. Tristique sollicitudin nibh sit amet. Eget nullam non nisi
+        est sit amet. Duis at consectetur lorem donec.
+      </p>
+      <p>
+        Massa tempor nec feugiat nisl. Auctor eu augue ut lectus arcu bibendum
+        at varius. Amet cursus sit amet dictum sit. Consectetur lorem donec
+        massa sapien faucibus. In nulla posuere sollicitudin aliquam ultrices.
+        Velit scelerisque in dictum non. In hac habitasse platea dictumst
+        quisque sagittis purus. Eget est lorem ipsum dolor sit amet consectetur
+        adipiscing elit. Mi in nulla posuere sollicitudin aliquam ultrices
+        sagittis. Congue eu consequat ac felis donec. Tortor dignissim convallis
+        aenean et tortor at.
+      </p>{" "}
+    </div>
+  );
+}
