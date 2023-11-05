aa98193d07ca195061b00ad0c09b54e8419162d8
brianslab <bdkuhn602@gmail.com>
2023-11-04 22:41:22 -0700

    create header

--- imdb/src/app/layout.jsx
+++ imdb/src/app/layout.jsx
@@ -1,9 +1,15 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {/* navbar */}
+        {/* search */}
+        {children}
+      </body>
     </html>
-  )
+  );
 }
